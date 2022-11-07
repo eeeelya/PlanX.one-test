827d6da85c33918a4e48dea88fa82770e35ee257
Goroshchik Ilya <goooroshchik@gmail.com>
2022-11-07 16:08:23 +0300

    Create api for category model and delete fixtures

--- cost_manager/category/models.py
+++ cost_manager/category/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+
+from core.abstract_models import SpecialInformation
+from user.models import User
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    custom = models.BooleanField(default=False)
+class Category(SpecialInformation):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(default="", max_length=50)
+    total_amount = models.DecimalField(
+        default=0, max_digits=10, decimal_places=2, validators=[MinValueValidator(0.00)]
+    )
 
     def __str__(self):
-        return str(self.name)
+        return f"{self.user} | {self.name}"
